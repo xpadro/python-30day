19426ca055c1545e9ae66ecffcaad86ac331896e
Xavier Padro <xpadro.sw@gmail.com>
2020-01-06 21:05:14 +0100

    day 21 generics

---
+++ d21-generics/generics.py
@@ -0,0 +1,7 @@
+'''
+There are no generics in Python. This exercise was implemented in Java 8:
+
+    public <T> void printArray(T[] arr) {
+        Arrays.asList(arr).forEach(System.out::println);
+    }
+'''
