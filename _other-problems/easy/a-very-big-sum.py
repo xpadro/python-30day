237efc5fde6acc0f4e95dd484df3dc6d7322abc0
Xavier Padro <xpadro.sw@gmail.com>
2020-01-16 22:32:18 +0100

    a very big sum problem

---
+++ _other-problems/easy/a-very-big-sum.py
@@ -0,0 +1,13 @@
+""" Calculate and print the sum of the elements in an array
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+"""
+
+def aVeryBigSum(ar):
+    return sum(ar)
+
+ar_count = int(input())
+ar = list(map(int, input().rstrip().split()))
+
+result = aVeryBigSum(ar)
+print(result)
