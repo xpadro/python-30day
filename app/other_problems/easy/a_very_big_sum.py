1192f9fee9e3c69ef8c77757a59a4b8eb6c378c8
Xavier Padro <xpadro.sw@gmail.com>
2020-01-17 17:31:04 +0100

    added description in a_very_big_sum

--- app/other_problems/easy/a_very_big_sum.py
+++ app/other_problems/easy/a_very_big_sum.py
@@ -1,8 +1,16 @@
 """ Calculate and print the sum of the elements in an array
 
 Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
-"""
 
+Input Format
+
+    The first line of the input consists of an integer n.
+    The next line contains n space-separated integers contained in the array.
+
+Output Format
+
+    Print the integer sum of the elements in the array.
+"""
 def aVeryBigSum(ar):
     return sum(ar)
 
