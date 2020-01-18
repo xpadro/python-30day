eec4dedfd79cac09a783e5bc2c79d6ce642015a3
Xavier Padro <xpadro.sw@gmail.com>
2020-01-18 19:36:05 +0100

    reordered folders

--- app/tests/algorithms/easy/test_plus_minus.py
+++ app/tests/algorithms/easy/test_plus_minus.py
@@ -1,4 +1,4 @@
-from other_problems.easy.plus_minus import plusMinus
+from algorithms.easy.plus_minus import plusMinus
 
 def test_plus_minus():
     assert plusMinus([1, 1, 0, -1, -1]) == [0.4, 0.4, 0.2], "Should be [0.4, 0.4, 0.2]"
--- app/tests/algorithms/easy/test_solve_me_first.py
+++ app/tests/algorithms/easy/test_solve_me_first.py
@@ -1,4 +1,4 @@
-from other_problems.easy.solve_me_first import solveMeFirst
+from algorithms.easy.solve_me_first import solveMeFirst
 
 def test_sum():
     assert solveMeFirst(2, 3) == 5, "Should be 5"
