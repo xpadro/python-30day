c839dfd2c60a5678bb773eba8734aa6b573a8291
Xavier Padro <xpadro.sw@gmail.com>
2020-01-18 21:23:24 +0100

    problem 2d arrays ds

--- app/data_structures/easy/_2d_arrays_ds.py
+++ app/data_structures/easy/_2d_arrays_ds.py
@@ -15,30 +15,13 @@ def max_hour_glass(arr):
 
             if max_hour_glass is None or current_hour_glass_sum > max_hour_glass:
                 max_hour_glass = current_hour_glass_sum
-            
-            print(str(arr[y][x]) + " " + str(arr[y][x+1]) + " " + str(arr[y][x+2]))
-            print("   " + str(arr[y+1][x+1]))
-            print(str(arr[y+2][x]) + " " + str(arr[y+2][x+1]) + " " + str(arr[y+2][x+2]))
-            print("")
     
     return max_hour_glass
 
+if __name__ == '__main__':
+    arr = []
 
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
 
-arr = []
-
-for _ in range(6):
-    arr.append(list(map(int, input().rstrip().split())))
-
-
-''' Test matrix:
-arr = [
-    [1, 2, 3, 4, 5, 6], 
-    [7, 8, 9, 10, 11, 12], 
-    [13, 14, 15, 16, 17, 18],
-    [19, 20, 21, 22, 23, 24],
-    [25, 26, 27, 28, 29, 30],
-    [31, 32, 33, 34, 35, 36]]
-'''
-
-print(max_hour_glass(arr))
+    print(max_hour_glass(arr))
---
+++ app/tests/data_structures/easy/test_2d_arrays_ds.py
@@ -0,0 +1,7 @@
+from data_structures.easy._2d_arrays_ds import max_hour_glass
+
+def test_max_hour_glass_sum():
+    assert max_hour_glass([[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 2, 4, 4, 0], [0, 0, 0, 2, 0, 0], [0, 0, 1, 2, 4, 0]]) == 19, "Should be 19"
+
+
+
