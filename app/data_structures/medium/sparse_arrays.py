378e6c0aed191025baa74cc782792d204e858cc6
Xavier Padro <xpadro.sw@gmail.com>
2020-02-09 23:32:53 +0100

    added doc string

--- app/data_structures/medium/sparse_arrays.py
+++ app/data_structures/medium/sparse_arrays.py
@@ -1,4 +1,11 @@
-# Complete the matchingStrings function below.
+'''
+There is a collection of input strings and a collection of query strings. 
+For each query string, determine how many times it occurs in the list of input strings.
+
+For example, given input strings = ['ab', 'ab', 'abc'] and queries = ['ab', 'abc', 'bc'], 
+we find 2 instances of 'ab', 1 of 'abc' and 0 of 'bc'. For each query, we add an element to out return array, 
+results = [2, 1, 0]
+'''
 def matchingStrings(strings, queries):
     result = []
 
