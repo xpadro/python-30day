7e3dad58aefbb47cd15785f5bee37d551e3fe102
Xavier Padro <xpadro.sw@gmail.com>
2020-05-27 19:30:18 +0200

    linkedlist implementation

---
+++ app/data_structures/linkedlist/linkedlist.py
@@ -0,0 +1,77 @@
+class LinkedListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        node = LinkedListNode(value)
+        self.head = node
+        self.tail = node
+        self.size = 1
+    
+    def append(self, value):
+        node = LinkedListNode(value)
+
+        self.tail.next = node
+        self.tail = node
+        self.size = self.size + 1
+
+    def prepend(self, value):
+        node = LinkedListNode(value)
+        node.next = self.head
+        self.head = node
+        self.size = self.size + 1
+
+    def insert(self, position, value):
+        if position == 0:
+            self.prepend(value)
+            return
+        
+        if position >= self.size:
+            self.append(value)
+            return
+        
+        found_node = self.__findPreviousNode(position)
+        
+        if found_node is not None:
+            node = LinkedListNode(value)
+            node.next = found_node.next
+            found_node.next = node
+            self.size = self.size + 1
+
+    def delete(self, position):
+        if position >= self.size:
+            return
+        
+        found_node = self.__findPreviousNode(position)
+
+        if found_node is not None:
+            to_delete = found_node.next
+            found_node.next = to_delete.next
+            to_delete = None
+            self.size = self.size - 1
+
+
+    def __findPreviousNode(self, position):
+        index = 0
+        current = self.head
+        previous = None
+
+        while index < position and current is not None:
+            previous = current
+            current = current.next
+            index = index + 1
+        
+        return previous
+
+    def print(self):
+        current = self.head
+        result = []
+
+        while current is not None:
+            result.append(current.value)
+            current = current.next
+        
+        return result
+
---
+++ app/tests/data_structures/linkedlist/test_linkedlist.py
@@ -0,0 +1,62 @@
+from data_structures.linkedlist.linkedlist import LinkedList, LinkedListNode
+
+def test_linkedlist():
+    linkedlist = LinkedList(5)
+
+    assert linkedlist.head.value == 5, "Should be 5"
+    assert linkedlist.size == 1, "Should be size of 1"
+
+def test_append():
+    linkedlist = LinkedList(5)
+    linkedlist.append(8)
+
+    assert linkedlist.head.value == 5, "Should be 5"
+    assert linkedlist.head.next.value == 8, "Should be 8"
+    assert linkedlist.tail.value == 8, "Should be 8"
+
+    assert linkedlist.size == 2, "Should be size of 2"
+    assert linkedlist.print() == [5, 8], "list should be [5, 8]"
+
+def test_prepend():
+    linkedlist = LinkedList(5)
+    linkedlist.prepend(8)
+
+    assert linkedlist.head.value == 8, "Should be 8"
+    assert linkedlist.head.next.value == 5, "head.next should be 5"
+    assert linkedlist.tail.value == 5, "tail should be 5"
+
+    assert linkedlist.size == 2, "Should be size of 2"
+    assert linkedlist.print() == [8, 5], "list should be [8, 5]"
+
+def test_insert():
+    linkedlist = LinkedList(1)
+    linkedlist.append(2)
+    linkedlist.append(3)
+    linkedlist.append(5)
+
+    linkedlist.insert(3, 4)
+
+    assert linkedlist.print() == [1, 2, 3, 4, 5], "list should be [1, 2, 3, 4, 5]"
+    assert linkedlist.size == 5, "Should be size of 5"
+
+def test_insert_bigger_index():
+    linkedlist = LinkedList(1)
+    linkedlist.append(2)
+    linkedlist.append(3)
+    linkedlist.append(5)
+
+    linkedlist.insert(99, 4)
+
+    assert linkedlist.print() == [1, 2, 3, 5, 4], "list should be [1, 2, 3, 5, 4]"
+    assert linkedlist.size == 5, "Should be size of 5"
+
+def test_delete():
+    linkedlist = LinkedList(1)
+    linkedlist.append(2)
+    linkedlist.append(3)
+    linkedlist.append(5)
+
+    linkedlist.delete(2)
+
+    assert linkedlist.print() == [1, 2, 5], "list should be [1, 2, 5]"
+    assert linkedlist.size == 3, "Should be size of 3"
