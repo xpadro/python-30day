08afa68ac8070fa7f232b420e6d6d383680f5b38
Xavier Padro <xpadro.sw@gmail.com>
2020-05-31 18:23:59 +0200

    binary search tree lookup

--- app/data_structures/tree/binary_search_tree.py
+++ app/data_structures/tree/binary_search_tree.py
@@ -15,25 +15,29 @@ class BinarySearchTree:
             self.root = toInsert
         else:
             self.__insertNode(toInsert, self.root)
-    
+
+    def lookup(self, value):
+        current_node = self.root
+
+        while current_node is not None:
+            if value < current_node.value:
+                current_node = current_node.left
+            elif value > current_node.value:
+                current_node = current_node.right
+            else:
+                return current_node.value
+        
         return None
 
     def __insertNode(self, toInsert, node):
-        print("Value: " + str(toInsert.value) + " | at node: " + str(node.value))
-
         if toInsert.value < node.value:
             if node.left is None:
-                print("insert value " + str(toInsert.value) + " at left node " + str(node.value))
                 node.left = toInsert
             else:
                 self.__insertNode(toInsert, node.left)
         else:
             if node.right is None:
-                print("insert value " + str(toInsert.value) + " at right node " + str(node.value))
                 node.right = toInsert
             else:
                 self.__insertNode(toInsert, node.right)
-        
-
-    def lookup(self, value):
-        None
+    
--- app/tests/data_structures/tree/test_binary_search_tree.py
+++ app/tests/data_structures/tree/test_binary_search_tree.py
@@ -1,6 +1,6 @@
 from data_structures.tree.binary_search_tree import BinarySearchTree, Node
 
-def test_binary_tree():
+def test_binary_tree_insert():
     tree = BinarySearchTree()
     tree.insert(10)
     tree.insert(8)
@@ -17,3 +17,17 @@ def test_binary_tree():
 
     # right subtree
     assert tree.root.right.value == 12, "root should be 12"
+
+def test_binary_tree_lookup():
+    tree = BinarySearchTree()
+    tree.insert(10)
+    tree.insert(8)
+    tree.insert(12)
+    tree.insert(4)
+    tree.insert(9)
+
+    assert tree.lookup(10) == 10, "Should have found 10"
+    assert tree.lookup(8) == 8, "Should have found 8"
+    assert tree.lookup(4) == 4, "Should have found 4"
+    assert tree.lookup(15) == None, "Should have not found the value"
+
