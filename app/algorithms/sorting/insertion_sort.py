2bdb78102109cea90b5d3095e094002c729f0b2a
Xavier Padro <xpadro.sw@gmail.com>
2020-06-02 19:25:32 +0200

    sorting insertion sort

---
+++ app/algorithms/sorting/insertion_sort.py
@@ -0,0 +1,15 @@
+#O(n^2). When list is almost sorted: O(n)
+def sort(arr):
+    length = len(arr)
+
+    for i in range(length):
+        if arr[i] < arr[0]:
+            arr.insert(0, arr.pop(i))
+
+        for j in range(1, i):
+            if arr[i] > arr[j-1] and arr[i] < arr[j]:
+                arr.insert(j, arr.pop(i))
+
+    return arr         
+
+
