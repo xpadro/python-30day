12a2d12cfa0c651e5d15d00adce1f0a0777dd55a
Xavier Padro <xpadro.sw@gmail.com>
2020-06-06 15:49:31 +0200

    fizz buzz problem

---
+++ app/algorithms/other/fizz_buzz.py
@@ -0,0 +1,19 @@
+'''
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. 
+For numbers which are multiples of both three and five output “FizzBuzz”.
+'''
+def fizz_buzz(number):
+    result = []
+
+    for i in range(1, number+1):
+        if i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    
+    return result
+
---
+++ app/tests/algorithms/other/test_fizz_buzz.py
@@ -0,0 +1,4 @@
+from algorithms.other.fizz_buzz import fizz_buzz
+
+def test_fizz_buzz():
+    assert fizz_buzz(5) == ["1", "2", "Fizz", "4", "Buzz"]
