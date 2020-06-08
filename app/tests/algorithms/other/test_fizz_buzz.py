779b14a90da421c9f8057aeb8c67d1a5700db4db
Xavier Padro <xpadro.sw@gmail.com>
2020-06-08 19:03:36 +0200

    added test cases

--- app/tests/algorithms/other/test_fizz_buzz.py
+++ app/tests/algorithms/other/test_fizz_buzz.py
@@ -1,5 +1,7 @@
 from algorithms.other.fizz_buzz import fizz_buzz
 
 def test_fizz_buzz():
+    assert fizz_buzz(1) == ["1"]
+    assert fizz_buzz(2) == ["1", "2"]
     assert fizz_buzz(5) == ["1", "2", "Fizz", "4", "Buzz"]
     assert fizz_buzz(15) == ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
