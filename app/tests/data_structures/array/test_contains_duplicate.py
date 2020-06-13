6ed3f1ad098fb10cdad8875733cc10d9c9c00c14
Xavier Padro <xpadro.sw@gmail.com>
2020-06-13 16:38:37 +0200

    added more test cases

--- app/tests/data_structures/array/test_contains_duplicate.py
+++ app/tests/data_structures/array/test_contains_duplicate.py
@@ -1,7 +1,9 @@
 from data_structures.array.contains_duplicate import contains_duplicate
 
 def test_contains_duplicate():
-    assert contains_duplicate([1,2,3,1]) == True, "Should be True"
-
-def test_does_not_contain_duplicate():
-    assert contains_duplicate([1,2,3,4]) == False, "Should be False"
+    assert contains_duplicate([]) == False
+    assert contains_duplicate([1]) == False
+    assert contains_duplicate([1,1]) == True
+    assert contains_duplicate([1,2,3,1]) == True
+    assert contains_duplicate([1,2,3,4]) == False
+    assert contains_duplicate([1,1,1,3,3,4,3,2,4,2]) == True
